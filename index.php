2b18eee57bea9a627466fdafdb2bc7d73913a3cb
Tetyana Sorokivska <sorokivska@gmail.com>
2017-01-22 18:16:21 +0200

    my first commit

---
+++ index.php
@@ -0,0 +1,387 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+  
+<!--[if IE 6]>
+<html id="ie6" lang="uk">
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" lang="uk">
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" lang="uk">
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+
+<!--<![endif]-->
+<title>IT moms design studio</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet"  href="css/style.css" type="text/css" media="all">
+  <link href="https://fonts.googleapis.com/css?family=Monoton" rel="stylesheet">
+</head>
+<body>
+<div id="wrap">
+ 
+<!--video 2-->
+ 
+ <video id="video_bg" autoplay="autoplay" loop="loop"> <!--video 2-->                                                         
+    <source src="/wp-content/uploads/2016/11/videoplayback.mp4" type="video/mp4"></source>
+    <source src="/wp-content/uploads/2016/11/videoplayback.webm" type="video/webm"></source>
+ </video> 
+
+
+
+<div class="jumbotron text-center"> 
+ 
+  <div class="container"> 
+      <p id="ITmoms"><span>IT moms</span></p>
+  <p id="studio"><span></span><span>web studio</span></p>
+  
+  </div>
+   
+</div>
+
+ 
+ 
+
+   <div id="about" class="container text-center">
+  <h3>IT moms design studio</h3>
+  <p><em>Про нас</em></p>
+  <p><p>Веб студія «IT moms design studio» пропонує  створення веб сайту з урахуванням останніх модних тенденцій інтернет-технологій.<br>
+Ми допоможемо створити сайт під ваш індивідуальний фірмовий стиль, адже сайт – це  обличчя фірми, або, іншими словами, це online представництво, яке відвідуватимуть постійні клієнти та зацікавлені вашими послугами, товарами чи діяльністю звичайні інтернет-користувачі. Грамотний підхід та функціональний сайт допоможе перетворити цих звичайних інтернет-користувачів в постійних клієнтів.</p>
+     <button class="btn btn-default btn-xs">Детальніше</button></p>
+  <br>
+  <div class="row">
+    <div class="col-sm-4">
+      <h4><strong>Марія</strong></h4><br>
+       <a href="#Maria" data-toggle="collapse">
+      <img src="/wp-content/uploads/2016/11/maria1.jpg" class="img-circle person" alt="Maria">
+       </a>
+       <div id="Maria" class="collapse">
+        <p>Бек-енд</p>
+        <p>Хоббі: книги й танці</p>
+        <p>якийсь текст</p>
+      </div>    
+    </div>
+
+    <div class="col-sm-4">
+      <h4><strong>Марта</strong></h4><br>
+      <a href="#Marta" data-toggle="collapse">
+      <img src="/wp-content/uploads/2016/11/marta1.jpg" class="img-circle person" alt="Marta">
+      </a>
+      <div id="Marta" class="collapse">
+        <p>Бек-енд</p>
+        <p>Хоббі: плавання й піші походи</p>
+        <p>якийсь текст</p>
+      </div>    
+    </div>
+
+    <div class="col-sm-4">
+      <h4><strong>Таня</strong></h4><br>
+      <a href="#Tanya" data-toggle="collapse">
+      <img src="/wp-content/uploads/2016/11/tanya-2.jpg" class="img-circle person" alt="Tanya">
+      </a>
+      <div id="Tanya" class="collapse">
+        <p>Фронт-енд</p>
+        <p>Хоббі: лежати в ліжку й щоб ніхто не рухав )))</p>
+        <p>якийсь текст</p>
+      </div>    
+    </div>
+  </div>
+</div>                                                     
+    
+
+
+
+
+
+
+<!--Початок блоку "Що ми робимо"-->
+<div id="works" class="container-fluid text-center">
+    <h2>Наші напрямки</h2>    
+    <br>
+    <div class="row">
+     <div class="col-md-4 col-sm-6">
+      <span class="glyphicon glyphicon-off logo-small"></span>
+      <h4>Корпоративний сайт</h4>
+      <p>Специфікою корпоративного сайту є його унікальність. Функціональні можливості такого сайту розробляються індивідуально згідно потреб вашої компанії. Особлива увага приділяється дизайну, адже це обличчя компанії, її імідж та ергономіці, яка являється запорукою високої конверсії простих відвідувачів сайту в клієнтів вашої компанії.</p>
+     </div>
+     <div class="col-md-4 col-sm-6">
+      <span class="glyphicon glyphicon-heart logo-small"></span>
+      <h4>Сайт візитка</h4>
+      <p>Сайт візитка – це простий, зручно структурований веб сайт, що містить кілька сторінок самої необхідної інформації, яка дозволяє користувачу швидко зорієнтуватись в напрямку вашої діяльності та стати вашим клієнтом.</p>
+    </div>
+    <div class="col-md-4 col-sm-6">
+      <span class="glyphicon glyphicon-lock logo-small"></span>
+      <h4>Представницький сайт</h4>
+      <p>Представницький сайт можна порівняти із сайтом візиткою, що презентує основну діяльність фірми в Інтернеті, але відрізняється додатковою функціональністю з широким спектром додаткових можливостей.</p>
+    </div>
+    </div>
+    <br><br>
+  <div class="row">
+    <div class="col-md-4 col-sm-6">
+      <span class="glyphicon glyphicon-leaf logo-small"></span>
+      <h4>Послуги художника</h4>
+      <p>Особливі замовлення потребують особливих рішень! Пропонуємо послуги художника, який нереальне зробить реальним, а веб – дизайнер перетворить це в комп’ютерну графіку. Мрії стають реальністю!</p>
+    </div>
+    <div class="col-md-4 col-sm-6">
+      <span class="glyphicon glyphicon-certificate logo-small"></span>
+      <h4>Послуги фотографа</h4>
+      <p>Якісний індивідуальний графічний контент Вашого сайту позитивно впливає на подальшу його розкрутку в пошукових системах та на позитивний імідж компанії серед користувачів сайту. У нас Ви маєте можливість замовити послуги професійного фотографа та послуги з подальшої обробки фотографій.</p>
+    </div>
+    <div class="col-md-4 col-sm-6">
+      <span class="glyphicon glyphicon-wrench logo-small"></span>
+      <h4>Логотип, фірмовий стиль</h4>
+      <p>Будь - яка компанія, яка нещодавно вийшла на ринок, чи вже довгий час функціонує, потребує позитивного іміджу, а загалом пізнаваності бренду серед численної кількості конкурентів та постійного його удосконалення. Розробка фірмового стилю - ключ до вирішення цього питання!</p>
+    </div>
+  </div>
+</div>
+<!--Кінець блоку "Що ми робимо"-->
+
+
+
+<!--Портфоліо-->
+<div id="portfolio" class="container-fluid text-center bg-grey "> <!-- bg-grey - можна забрати, тобі краще буде-->
+  <h2>Портфоліо</h2>
+  <h4>Наші роботи</h4>
+  <div class="row text-center">
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="/wp-content/uploads/2016/11/web_design3_img.jpg" alt="сайт1" >
+        <p><strong>сайт1</strong></p>
+        <p>виконаний в мінімалістичному стилі для підприємтсва, що займається сільським господарством. Орієнтація - сайт-візитка для залучення великих гуртових покупців</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="/wp-content/uploads/2016/11/web-design1-1.jpg" alt="сайт2">
+        <p><strong>сайт2</strong></p>
+        <p>виконаний в стилі luxury для салону краси. Орієнтація - сайт-візитка для залучення роздрібних клієнтів</p>
+      </div>
+    </div>
+    <div class="col-sm-4">
+      <div class="thumbnail">
+        <img src="/wp-content/uploads/2016/11/web-design2.jpg" alt="сайт3">
+        <p><strong>сайт3</strong></p>
+        <p>виконаний в стилі techno музичної групи. Орієнтація  - представницький сайт</p>
+      </div>
+    </div>
+</div>
+
+
+<!--Карусель-->
+<h2>Відгуки клієнтів</h2>
+<div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
+  <!-- Індикатори -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Обгортка для слайдів -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+    <h4>"Сайт получився саме такий, який я й очікувала. Мої продажі виросли на 50%!"<br><span style="font-style:normal;">Марта</span></h4>
+    </div>
+    <div class="item">
+      <h4>"Я просто в захопленні!"<br><span style="font-style:normal;">Марічка</span></h4>
+    </div>
+    <div class="item">
+      <h4>"З IT moms design Studio приємно мати справу. Все виконано в терміни. Дівчата дуже приємні у спілкуванні :)"<br>     <span style="font-style:normal;">Таня</span></h4>
+     </div>
+     <div class="item">
+      <h4>"З IT moms design Studio бажаю успіхів!"<br><span style="font-style:normal;">Юра</span></h4>
+     </div> 
+   </div>
+ 
+
+  <!-- Лівий й правий перемикач -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<!--кінець каруселі-->
+
+<div id="blog" class="container-fluid bg-grey">
+ <div class="row">
+  <div class="col-sm-12">   
+     <h2>Блог</h2>
+     <h4><strong>наприклад</strong>  Місія - це уявлення фірми про те, заради чого вона існує, що виробляє і для кого, до чого   прагне.</h4> 
+     <p><strong>Ціль:</strong> При розробці місії необхідно передусім усвідомити, який саме бізнес, в якому діє компанія. Це означає відповідь на питання: які потреби задовольняє фірма? чиї потреби (якого сегменту ринку) задовольняє фірма? які технології (у широкому сенсі) при цьому використовуються?
+На основі місії розробляються і приймаються цілі фірми. На відміну від місії, цілей може бути одночасно декілька, і кожна приймається на конкретний термін. Цілі завжди формулюються так, щоб згодом можна було сказати, виконана ціль чи ні.
+При розробці місії необхідно передусім усвідомити, який саме бізнес, в якому діє компанія. Це означає відповідь на питання: які потреби задовольняє фірма? чиї потреби (якого сегменту ринку) задовольняє фірма? які технології (у широкому сенсі) при цьому використовуються?
+На основі місії розробляються і приймаються цілі фірми. На відміну від місії, цілей може бути одночасно декілька, і кожна приймається на конкретний термін. Цілі завжди формулюються так, щоб згодом можна було сказати, виконана ціль чи ні.При розробці місії необхідно передусім усвідомити, який саме бізнес, в якому діє компанія. Це означає відповідь на питання: які потреби задовольняє фірма? чиї потреби (якого сегменту ринку) задовольняє фірма? які технології (у широкому сенсі) при цьому використовуються?
+На основі місії розробляються і приймаються цілі фірми. На відміну від місії, цілей може бути одночасно декілька, і кожна приймається на конкретний термін. Цілі завжди формулюються так, щоб згодом можна було сказати, виконана ціль чи ні.
+При розробці місії необхідно передусім усвідомити, який саме бізнес, в якому діє компанія. Це означає відповідь на питання: які потреби задовольняє фірма? чиї потреби (якого сегменту ринку) задовольняє фірма? які технології (у широкому сенсі) при цьому використовуються?</p>
+  </div>
+ </div>
+</div>
+
+
+
+<!--Початок контактів-->
+
+<div id="contacts" class="container-fluid bg-grey">
+  <h2 class="text-center">КОНТАКТИ</h2>
+  <div class="row">
+    <div class="col-sm-5">
+      <p>Зв'яжіться з нами!</p>
+      <p><span class="glyphicon glyphicon-map-marker"></span> Львів, Україна</p>
+      <p><span class="glyphicon glyphicon-phone"></span> +0380950000</p>
+      <p><span class="glyphicon glyphicon-envelope"></span> myemail@gmail.com</p> 
+    </div>
+    <div class="col-sm-7">
+      <div class="row">
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="name" name="name" placeholder="Ім'я" type="text" required>
+        </div>
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+        </div>
+      </div>
+      <textarea class="form-control" id="comments" name="comments" placeholder="Коментарі" rows="5"></textarea><br>
+      <div class="row">
+        <div class="col-sm-12 form-group">
+          <button class="btn btn-default pull-right" type="submit">Відправити</button>
+        </div>
+      </div> 
+    </div>
+  </div>
+</div>
+
+<!--кінець контактів-->
+
+
+
+
+<!--Scrollspy-->
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
+
+
+
+<!--Хідер, меню-->
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="#myPage">LOGO</a>                      
+    </div>
+
+
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">                    <!--З правого боку навігація-->
+        <li><a href="#about"><strong>ПРО НАС</strong></a></li>
+
+
+<!--випадаюче меню
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#our">НАШІ НАПРЯМКИ
+            <span class="caret"></span>
+          </a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Корпоративні сайти</a></li>
+            <li><a href="#">Сайт-візитка</a></li>
+            <li><a href="#">Представницький сайт</a></li> 
+          </ul>
+       </li>
+кінець випадаючого меню-->
+
+        <li><a href="#works"><strong>НАШІ НАПРЯМКИ</strong></a></li>
+        <li><a href="#portfolio"><strong>ПОРТФОЛІО</strong></a></li>
+        <li><a href="#blog"><strong>БЛОГ</strong></a></li>        
+        <li><a href="#contacts"><strong>КОНТАКТИ</strong></a></li>
+        <form class="navbar-form navbar-left">
+        <li><a href="#"><span class="glyphicon glyphicon-search"></span></a></li>
+
+<!--форма для поля з пошуком
+   <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search">
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>-->             
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</div>
+
+<!--Футер-->
+<div id="footer" class="container-fluid">
+<footer class="text-center">
+  <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="ВГОРУ">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a><br><br>
+
+  <?php echo __('© ', '')   .'  '.date(Y);  ?>
+
+
+  <div>IT moms web studio<a href="#myPage" data-toggle="tooltip" title="наш сайт">  www.sorokivska.intech.lviv.ua</a></div> 
+</footer>
+
+<script>
+$(document).ready(function(){
+    // Initialize Tooltip
+    $('[data-toggle="tooltip"]').tooltip(); 
+})
+</script>
+   
+</div>
+
+<!--кінець Футер-->
+
+<!--Додаємо Smooth Scrolling-->
+<script>
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
+
+  // Make sure this.hash has a value before overriding default behavior
+  if (this.hash !== "") {
+
+    // Prevent default anchor click behavior
+    event.preventDefault();
+
+    // Store hash
+    var hash = this.hash;
+
+    // Using jQuery's animate() method to add smooth page scroll
+    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 900, function(){
+
+      // Add hash (#) to URL when done scrolling (default click behavior)
+      window.location.hash = hash;
+      });
+    } // End if 
+  });
+})
+</script>
+
+<!--кінець Smooth Scrolling-->
+
+
+
+
+
+
+</body>
+</html>
